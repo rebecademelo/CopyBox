a428bfc3c656a844fb15f09a7605d28461713662
rebecademelo <rebeca_mss@hotmail.com>
2019-05-23 22:31:33 -0300

    copia do dropbox

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect(
+    'mongodb+srv://rebeca:kcce@copybox-fe6o4.mongodb.net/copybox?retryWrites=true', 
+    {
+    useNewUrlParser: true
+    }
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : true}));
+
+app.use(require('./routes'));
+
+app.listen(3333);
